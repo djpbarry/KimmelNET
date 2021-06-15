364f48f9b17be26bfd0ab76d537c09d6d9060841
Dave Barry <david.barry@crick.ac.uk>
2021-06-15 18:31:50 +0100

    Succesful implementation of binary classifier based on AlexNet - validation accuracy is a bit erratic

--- alexnet_classifier.py
+++ alexnet_classifier.py
@@ -4,45 +4,69 @@ import matplotlib.pyplot as plt
 import os
 import time
 
-image_size = (227, 227)
-batch_size = 32
+image_size = (411, 491)
+batch_size = 64
+num_classes = 2
+epochs = 200
+buffer_size = 2
 
-#(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+# (train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
 
-#CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+# CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-#validation_images, validation_labels = train_images[:5000], train_labels[:5000]
-#train_images, train_labels = train_images[5000:], train_labels[5000:]
+# validation_images, validation_labels = train_images[:5000], train_labels[:5000]
+# train_images, train_labels = train_images[5000:], train_labels[5000:]
 
-#train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-#test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-#validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
+# train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+# test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+# validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "Zebrafish",
+    "Zebrafish_Train",
     validation_split=0.2,
     subset="training",
     seed=1337,
     image_size=image_size,
     batch_size=batch_size,
+    shuffle=True,
+    color_mode="grayscale",
+    labels="inferred",
+    label_mode="binary"
 )
 
 validation_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "Zebrafish",
+    "Zebrafish_Train",
     validation_split=0.2,
     subset="validation",
     seed=1337,
     image_size=image_size,
     batch_size=batch_size,
+    shuffle=True,
+    color_mode="grayscale",
+    labels="inferred",
+    label_mode="binary"
 )
 
-#plt.figure(figsize=(20,20))
-#for i, (image, label) in enumerate(train_ds.take(5)):
-#    ax = plt.subplot(5,5,i+1)
-#    plt.imshow(image)
-#    plt.title(CLASS_NAMES[label.numpy()[0]])
-#    plt.axis('off')
-#plt.show()
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "Zebrafish_Test",
+    image_size=image_size,
+    batch_size=batch_size,
+    color_mode="grayscale",
+    labels="inferred",
+    label_mode="binary"
+)
+
+train_ds = train_ds.cache().prefetch(buffer_size=buffer_size)
+validation_ds = validation_ds.cache().prefetch(buffer_size=buffer_size)
+
+plt.figure(figsize=(17, 20))
+for images, labels in train_ds.take(1):
+    for i in range(25):
+        ax = plt.subplot(5, 5, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+plt.savefig('sample_images.png')
 
 
 def process_images(image, label):
@@ -53,75 +77,94 @@ def process_images(image, label):
     return image, label
 
 
-#train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
-#test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
-#validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
+# test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
+# validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
 
-#train_ds_size_1 = tf.data.experimental.cardinality(train_ds_1).numpy()
-#validation_ds_size_1 = tf.data.experimental.cardinality(validation_ds_1).numpy()
+# train_ds_size_1 = tf.data.experimental.cardinality(train_ds_1).numpy()
+# validation_ds_size_1 = tf.data.experimental.cardinality(validation_ds_1).numpy()
 
-#print("Training data size:", train_ds_size)
-#print("Test data size:", test_ds_size)
-#print("Validation data size:", validation_ds_size)
+# print("Training data size:", train_ds_size)
+# print("Test data size:", test_ds_size)
+# print("Validation data size:", validation_ds_size)
 
-#train_ds = (train_ds.map(process_images).shuffle(buffer_size=train_ds_size).batch(batch_size=batch_size, drop_remainder=True))
-#test_ds = (test_ds.map(process_images).shuffle(buffer_size=train_ds_size).batch(batch_size=32, drop_remainder=True))
-#validation_ds = (validation_ds.map(process_images).shuffle(buffer_size=train_ds_size).batch(batch_size=batch_size, drop_remainder=True))
+# train_ds = (train_ds.map(process_images).shuffle(buffer_size=train_ds_size).batch(batch_size=batch_size, drop_remainder=True))
+# test_ds = (test_ds.map(process_images).shuffle(buffer_size=train_ds_size).batch(batch_size=32, drop_remainder=True))
+# validation_ds = (validation_ds.map(process_images).shuffle(buffer_size=train_ds_size).batch(batch_size=batch_size, drop_remainder=True))
 
-root_logdir = os.path.join(os.curdir, "logs\\fit\\")
-
-
-def get_run_logdir():
-    run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
-    return os.path.join(root_logdir, run_id)
+# root_logdir = os.path.join(os.curdir, "logs\\fit\\")
 
 
-run_logdir = get_run_logdir()
-tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+# def get_run_logdir():
+#    run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
+#    return os.path.join(root_logdir, run_id)
 
-#strategy = tf.distribute.MirroredStrategy()
 
-#with strategy.scope():
+# run_logdir = get_run_logdir()
+# tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
 
-data_augmentation = keras.Sequential(
-    [
-        keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
-        keras.layers.experimental.preprocessing.RandomFlip("vertical"),
-    ]
-)
+strategy = tf.distribute.MirroredStrategy()
 
-inputs = keras.Input(shape=image_size+(3,))
-x = data_augmentation(inputs)
-x = keras.layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
-x = keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3))(x)
-x = keras.layers.BatchNormalization()(x)
-x = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
-x = keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same")(x)
-x = keras.layers.BatchNormalization()(x)
-x = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
-x = keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same")(x)
-x = keras.layers.BatchNormalization()(x)
-x = keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same")(x)
-x = keras.layers.BatchNormalization()(x)
-x = keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same")(x)
-x = keras.layers.BatchNormalization()(x)
-x = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
-x = keras.layers.Flatten()(x)
-x = keras.layers.Dense(4096, activation='relu')(x)
-x = keras.layers.Dropout(0.5)(x)
-x = keras.layers.Dense(4096, activation='relu')(x)
-x = keras.layers.Dropout(0.5)(x)
-outputs = keras.layers.Dense(5, activation='softmax')(x)
+with strategy.scope():
+    data_augmentation = keras.Sequential(
+        [
+            keras.layers.experimental.preprocessing.RandomFlip(mode="horizontal_and_vertical"),
+            keras.layers.experimental.preprocessing.RandomTranslation(0.2, 0.2, fill_mode="reflect",
+                                                                      interpolation="bilinear"),
+            keras.layers.experimental.preprocessing.RandomRotation(1.0, fill_mode="reflect", interpolation="bilinear")
+        ]
+    )
 
-model = keras.Model(inputs, outputs)
+    inputs = keras.Input(shape=image_size + (1,))
+    x = data_augmentation(inputs)
+    x = keras.layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
+    x = keras.layers.Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation='relu', padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
+    x = keras.layers.Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
+    x = keras.layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(4096, activation='relu')(x)
+    x = keras.layers.Dropout(0.5)(x)
+    x = keras.layers.Dense(4096, activation='relu')(x)
+    x = keras.layers.Dropout(0.5)(x)
+    # if num_classes == 2:
+    #    activation = "sigmoid"
+    # else:
+    #    activation = "softmax"
+    # outputs = keras.layers.Dense(num_classes, activation=activation)(x)
+    outputs = keras.layers.Dense(1, activation="sigmoid")(x)
+    model = keras.Model(inputs, outputs)
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.optimizers.SGD(lr=0.001), metrics=['accuracy'])
-#model.summary()
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(train_ds,
-          epochs=1,
-          validation_data=validation_ds,
-          validation_freq=1,
-          callbacks=[tensorboard_cb])
+history = model.fit(train_ds,
+                    epochs=epochs,
+                    validation_data=validation_ds,
+                    validation_freq=1)
 
 model.save('trained_alexnet_model')
+
+plt.figure(figsize=(20, 10))
+plt.subplot(1, 2, 1)
+plt.suptitle('Optimizer : Adam', fontsize=10)
+plt.ylabel('Loss', fontsize=16)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend(loc='upper right')
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy', fontsize=16)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.savefig('training_progress.png')
+
+model.evaluate(test_ds)
