7546ab9a41bab8e5ca92852c393045567e20e0f4
Dave Barry <david.barry@crick.ac.uk>
2021-07-20 21:16:36 +0100

    Simplified definition of training/validation data using tf.data API

--- alexnet_regression.py
+++ alexnet_regression.py
@@ -1,80 +1,37 @@
-import datetime
 import os
 
 import matplotlib.pyplot as plt
-import numpy
-import pandas
+import tensorflow as tf
 from tensorflow import keras
 
 image_size = (190, 227)
-batch_size = 128
+batch_size = 32
 # num_classes = 5
 epochs = 200
-# buffer_size = 2
-train_path = "Z:/working/barryd/hpc/python/keras_image_class/Zebrafish_Train_Regression"
-test_path = "Z:/working/barryd/hpc/python/keras_image_class/Zebrafish_Test_Regression"
+buffer_size = 4
+# train_path = "Z:/working/barryd/hpc/python/keras_image_class/Zebrafish_Train_Regression"
+# test_path = "Z:/working/barryd/hpc/python/keras_image_class/Zebrafish_Test_Regression"
 
 
-# train_path = "Zebrafish_Train_Regression"
-# test_path = "Zebrafish_Test_Regression"
+train_path = "Zebrafish_Train_Regression"
+test_path = "Zebrafish_Test_Regression"
 
 
-def load_images(inputDir, size):
-    # images = []
-    paths = []
-    start = len(inputDir) + 1
-    for folder_name in os.listdir(inputDir):
-        current_dir = os.path.join(inputDir, folder_name)
-        print(str(datetime.datetime.now()) + ": Processing " + current_dir + " - Number of files: " + str(len(os.listdir(current_dir))))
-        for fname in os.listdir(current_dir):
-            fpath = os.path.join(current_dir, fname)
-            #            image = skio.imread(fpath)
-            #            image = skt.resize(image, size)
-            #            images.append(image)
-            hpfs = float(fpath[start:fpath.index("\\", start)])
-            paths.append({'File': fpath, 'hpfs': hpfs})
-    # return numpy.array(images), paths
-    return paths
+def parse_image(filename):
+    parts = tf.strings.split(filename, os.sep)
+    label = float(parts[-2])
+    image = tf.io.read_file(filename)
+    image = tf.image.decode_png(image)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, image_size)
+    return image, label
 
 
-# train_images, train_paths = load_images(train_path, image_size)
-train_paths = load_images(train_path, image_size)
-start = len(train_path) + 1
-# hpfs = numpy.array([[float(f[start:index]) for index in [f.index("\\", start)]] for f in train_paths])
-
-inputDataFrame = pandas.DataFrame(train_paths)
-
-plt.hist(inputDataFrame.hpfs, bins=190)
-plt.show()
-
-# print(inputDataFrame[1:20])
-
-train_datagen = keras.preprocessing.image.ImageDataGenerator(validation_split=0.2)
-
-trainImageGen = train_datagen.flow_from_dataframe(inputDataFrame, x_col='File', y_col='hpfs', target_size=image_size,
-                                                  class_mode='raw', color_mode='grayscale', batch_size=batch_size,
-                                                  subset="training", directory=None)
-
-valImageGen = train_datagen.flow_from_dataframe(inputDataFrame, x_col='File', y_col='hpfs', target_size=image_size,
-                                                class_mode='raw', color_mode='grayscale', batch_size=batch_size,
-                                                subset="validation", directory=None)
-# train_images, val_images, train_hpfs, val_hpfs = sklms.train_test_split(train_images, hpfs, test_size=0.2)
-
-# auto = tf.data.AUTOTUNE
-# train_ds = train_ds.prefetch(buffer_size=auto).cache()
-# test_ds = test_ds.prefetch(buffer_size=auto).cache()
-
-# plt.figure(figsize=(20, 20))
-# for images, labels in train_ds.take(1):
-#    for i in range(25):
-#    ax = plt.subplot(5, 5, i + 1)
-#   plt.imshow(images[i].numpy().astype("uint8"))
-#    for j in range(len(CLASS_NAMES)):
-#         if labels.numpy()[i][j] > 0:
-#              plt.title(CLASS_NAMES[j])
-#       plt.axis("off")
-# plt.savefig('sample_images.png')
-
+list_ds = tf.data.Dataset.list_files(str(train_path + os.sep + "*" + os.sep + "*.png")).shuffle(1000)
+images_ds = list_ds.map(parse_image).batch(batch_size)
+val_split = int(0.2 * len(images_ds))
+val_ds = images_ds.take(val_split).prefetch(buffer_size).cache()
+train_ds = images_ds.skip(val_split).prefetch(buffer_size).cache()
 
 # strategy = tf.distribute.MirroredStrategy()
 
@@ -118,33 +75,33 @@ model.compile(
     optimizer=keras.optimizers.Adam()
 )
 
-history = model.fit(trainImageGen,
+history = model.fit(train_ds,
                     epochs=epochs,
                     validation_freq=1,
-                    validation_data=valImageGen)
+                    validation_data=val_ds)
 
-#print(history.history.keys())
+# print(history.history.keys())
 
 model.save('trained_alexnet_regression_model')
 
 plt.figure(figsize=(20, 10))
-#plt.subplot(1, 2, 1)
-#plt.suptitle('Optimizer : Adam', fontsize=10)
+# plt.subplot(1, 2, 1)
+# plt.suptitle('Optimizer : Adam', fontsize=10)
 plt.title('Optimizer : Adam', fontsize=10)
 plt.ylabel('Loss', fontsize=16)
 plt.plot(history.history['loss'], label='Training Loss')
 plt.plot(history.history['val_loss'], label='Validation Loss')
 plt.legend(loc='upper right')
 
-#plt.subplot(1, 2, 2)
-#plt.ylabel('Mean Absolute Error', fontsize=16)
-#plt.plot(history.history['mean_absolute_error'], label='Training Error')
-#plt.plot(history.history['val_mean_absolute_error'], label='Validation Error')
-#plt.legend(loc='lower right')
+# plt.subplot(1, 2, 2)
+# plt.ylabel('Mean Absolute Error', fontsize=16)
+# plt.plot(history.history['mean_absolute_error'], label='Training Error')
+# plt.plot(history.history['val_mean_absolute_error'], label='Validation Error')
+# plt.legend(loc='lower right')
 plt.savefig('regression_training_progress.png')
 
-#test_images, test_paths = load_images(test_path, image_size)
-#start = len(test_path) + 1
-#test_hpfs = numpy.array([[float(f[start:index]) for index in [f.index("/", start)]] for f in test_paths])
+# test_images, test_paths = load_images(test_path, image_size)
+# start = len(test_path) + 1
+# test_hpfs = numpy.array([[float(f[start:index]) for index in [f.index("/", start)]] for f in test_paths])
 
-#model.evaluate(test_images, test_hpfs)
+# model.evaluate(test_images, test_hpfs)
