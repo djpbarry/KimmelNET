18c4e9f93463d744f9c9a0e9f25db9e67057dd82
David Barry <david.barry@crick.ac.uk>
2021-08-13 13:39:03 +0100

    Added files for hypertuning

---
+++ hypertune_simple_regression.py
@@ -0,0 +1,71 @@
+import os
+
+import tensorflow as tf
+from keras_tuner import RandomSearch
+from tensorflow import keras
+from tensorflow.keras import layers
+
+image_size = (112, 134)
+cropped_image_size = (112, 112)
+batch_size = 256
+epochs = 500
+buffer_size = 4
+train_path = "Zebrafish_Train_Regression"
+test_path = "Zebrafish_Test_Regression"
+name = "hypertune_simple_regression"
+
+
+def parse_image(filename):
+    parts = tf.strings.split(filename, os.sep)
+    label = float(parts[-2])
+    image = tf.io.read_file(filename)
+    image = tf.image.decode_png(image)
+    image = tf.image.resize(image, image_size)
+    return image, label
+
+
+def build_model(hp):
+    model = keras.Sequential()
+    model.add(keras.Input(shape=image_size + (1,)))
+    model.add(layers.experimental.preprocessing.RandomFlip(mode="horizontal_and_vertical"))
+    model.add(layers.experimental.preprocessing.Rescaling(1.0 / 255))
+    model.add(layers.experimental.preprocessing.CenterCrop(cropped_image_size[0], cropped_image_size[1]))
+    for i in range(hp.Int("num_layers", 2, 10)):
+        model.add(layers.Conv2D(hp.Int("units_" + str(i), min_value=16, max_value=256, step=16), kernel_size=(3, 3),
+                                activation="relu"))
+        model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1))
+    model.compile(
+        loss="mean_squared_error", optimizer="adam"
+    )
+    return model
+
+
+train_list_ds = tf.data.Dataset.list_files(str(train_path + os.sep + "*" + os.sep + "*.png")).shuffle(1000)
+train_images_ds = train_list_ds.map(parse_image).batch(batch_size)
+val_split = int(0.2 * len(train_images_ds))
+val_ds = train_images_ds.take(val_split).prefetch(buffer_size).cache()
+train_ds = train_images_ds.skip(val_split).prefetch(buffer_size).cache()
+train_ds = train_ds.prefetch(buffer_size=buffer_size).cache()
+val_ds = val_ds.prefetch(buffer_size=buffer_size).cache()
+test_list_ds = tf.data.Dataset.list_files(str(test_path + os.sep + "*" + os.sep + "*.png")).shuffle(1000)
+test_ds = test_list_ds.map(parse_image).batch(batch_size)
+test_ds = test_ds.prefetch(buffer_size=buffer_size).cache()
+
+tuner = RandomSearch(
+    build_model,
+    objective="val_accuracy",
+    max_trials=10,
+    executions_per_trial=3,
+    overwrite=True,
+    directory="outputs",
+    project_name=name,
+)
+
+tuner.search_space_summary()
+
+tuner.search(train_ds, epochs=epochs, validation_data=val_ds, validation_freq=1)
+
+tuner.results_summary()
